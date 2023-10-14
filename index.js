188be5d20b63f1f6d195e08c7ce8531ce1c3d434
isaacgichuhi <zacmuturi45@gmail.com>
2023-10-14 11:37:07 +0300

    added explanations

--- index.js
+++ index.js
@@ -9,18 +9,26 @@ function hasTargetSum(array, target) {
     }
   }
     return false;
-}
+};
 
 /* 
   Write the Big O time complexity of your function here
+  0(n²)
 */
 
 /* 
   Add your pseudocode here
+  First, loop through the array to get elements. Then loop again but modify a nested loop which starts at one element
+  ahead of the first loop. Get the complement of the target and first loop's elements. Then with an if statement, look
+  for an element in the second loop that is equal to the complement. 
 */
 
 /*
   Add written explanation of your solution here
+  This function loops through the array twice. The second loop is one element ahead of the first one.
+  This ensures that all the elements in the array can be added to each other. We then get the complement of the target
+  and the elements in the first loop. If there is an element in the second loop equal to this complement, then the function returns true.
+  If the array reaches the end of the loop without finding this element it returns false. 
 */
 
 // You can run `node index.js` to view these console logs
